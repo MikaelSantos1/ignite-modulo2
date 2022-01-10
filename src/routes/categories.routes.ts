6c0feb4d81e08d06fd06f798def6850db174096f
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-10 17:00:10 -0300

    created specification repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 
-import { CategoriesRepositiry } from "../repositories/CategoryRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoryRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoryRoutes = Router();
-const categoriesRepositiry = new CategoriesRepositiry();
+const categoriesRepositiry = new CategoriesRepository();
 
 categoryRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
