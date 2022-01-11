1ba13953fa53a5a4e2690505717241dfb31b840f
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-11 17:40:29 -0300

    multer add

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCase/createCategory";
+import { importCategoryController } from "../modules/cars/useCase/importCategory";
 import { listCategoryController } from "../modules/cars/useCase/listCategory";
 
+const upload = multer({
+    dest: "./tmp",
+});
 const categoryRoutes = Router();
 
 categoryRoutes.post("/", (request, response) => {
@@ -11,5 +16,9 @@ categoryRoutes.post("/", (request, response) => {
 
 categoryRoutes.get("/", (request, response) => {
     return listCategoryController.handle(request, response);
+});
+
+categoryRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response);
 });
 export { categoryRoutes };
