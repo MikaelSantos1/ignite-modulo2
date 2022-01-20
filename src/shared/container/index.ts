59e3a8b4c7ac6375f96bc8fb4e8431e71c91415d
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-20 16:58:04 -0300

    specfication feature

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,17 +1,16 @@
-import {container} from 'tsyringe'
+import { container } from "tsyringe";
 
-import {ICategoriesRepository} from '../../modules/cars/repositories/ICategoriesRepository'
-import {ISpecificationRepository} from '../../modules/cars/repositories/ISpecificationRepository'
-
-import {CategoriesRepository}from '../../modules/cars/repositories/implemantations/CategoryRepository'
-import { SpecificationRepository}from '../../modules/cars/repositories/implemantations/SpecificationRepository'
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implemantations/CategoryRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/implemantations/SpecificationRepository";
+import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
-)
+);
 
 container.registerSingleton<ISpecificationRepository>(
     "SpecificationRepository",
     SpecificationRepository
-)
+);
