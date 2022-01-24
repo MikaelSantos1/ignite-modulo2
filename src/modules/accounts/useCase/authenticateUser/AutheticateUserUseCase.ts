fe129891b930215d8279645656cd57f5c55875ea
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-24 15:34:38 -0300

    handle errors

--- src/modules/accounts/useCase/authenticateUser/AutheticateUserUseCase.ts
+++ src/modules/accounts/useCase/authenticateUser/AutheticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUserRepository } from "../../repository/IUsersRepository";
 
 interface IRequest {
@@ -26,12 +27,12 @@ class AuthenticateUserUsecase {
         const user = await this.usersRepository.findByEmail(email);
 
         if (!user) {
-            throw new Error("Email or password incorrect");
+            throw new AppError("Email or password incorrect");
         }
         const passwordMatch = compare(password, user.password);
 
         if (!passwordMatch) {
-            throw new Error("Email or password incorrect");
+            throw new AppError("Email or password incorrect");
         }
         const token = sign({}, "024d271e5567f17bbb89306b9ad7845c", {
             subject: user.id,
