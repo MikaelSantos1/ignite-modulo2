df59310a9c1d03c95a332048e089f350c1d07873
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-01-26 14:25:44 -0300

    avatar delete from paste

--- src/modules/accounts/useCase/updateUsersAvatar/UpdateUsersAvatarUseCase.ts
+++ src/modules/accounts/useCase/updateUsersAvatar/UpdateUsersAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUserRepository } from "../../repository/IUsersRepository";
 
 interface IRequest {
@@ -15,6 +16,7 @@ class UpdateUsersAvatarUseCase {
     ) {}
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
+        await deleteFile(`./tmp/avatar/${user.avatar}`);
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
